d5f8bf0f9acd1dce2399a4cbd286a96d3e9caf3f
Rasmus Elmgren <elmgren93@gmail.com>
2016-04-28 17:55:08 +0200

    added marker animation to bus-button

--- map.js
+++ map.js
@@ -3,6 +3,25 @@ var peopleToggler = false;
 var roadToggler = false;
 var busToggler = false;
 
+var markers = [];
+var busMarkers = [
+  {lat: 59.338, lng: 18.065},
+  {lat: 59.339, lng: 18.065},
+  {lat: 59.340, lng: 18.065},
+  {lat: 59.341, lng: 18.065},
+  {lat: 59.342, lng: 18.065},
+  {lat: 59.343, lng: 18.065},
+  {lat: 59.344, lng: 18.065},
+  {lat: 59.345, lng: 18.065},
+  {lat: 59.345, lng: 18.064},
+  {lat: 59.345, lng: 18.063},
+  {lat: 59.345, lng: 18.062},
+  {lat: 59.345, lng: 18.061},
+  {lat: 59.345, lng: 18.060},
+  {lat: 59.346, lng: 18.061}
+];
+
+
 
 function initMap() {
   var mapDiv = document.getElementById('map');
@@ -55,21 +74,26 @@ function roadToggle() {
 }
 
 function busToggle() {
-  if ( busToggler == false ) {
-    document.getElementsByClassName('button')[2].style.backgroundColor = "black";
-    busMarker = new google.maps.Marker({
-      map: map,
-      draggable: false,
-      animation: google.maps.Animation.DROP,
-      position: {lat: 59.338, lng: 18.065},
-      icon: 'bus.png'
-    });
-    busToggler = true;
-  } else {
-    busMarker.setMap(null);
-    document.getElementsByClassName('button')[2].style.backgroundColor = "transparent";
-    busToggler = false;
+  clearMarkers();
+  for (var i = 0; i < busMarkers.length; i++) {
+    addMarkerWithTimeout(busMarkers[i], i * 200);
   }
+}
+
+function addMarkerWithTimeout(position, timeout) {
+  window.setTimeout(function() {
+    markers.push(new google.maps.Marker({
+      position: position,
+      map: map,
+    }));
+  }, timeout);
+}
+
+function clearMarkers() {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
 }
 
 function toggleInfo() {
